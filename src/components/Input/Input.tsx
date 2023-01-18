fec57ea8ce84edd6b70d5cc8f27a244e10e4bbc1
Cristina <cristinaforescampos1992@gmail.com>
2023-01-18 18:47:46 +0100

    Create component Intput

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface InputProps {
+  type: React.HTMLInputTypeAttribute;
+  placeholder: string;
+  htmlFor: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  required?: boolean;
+  autoComplete?: string;
+  id?: string;
+  textLabel: string;
+}
+
+const Input = ({
+  type,
+  placeholder,
+  htmlFor,
+  onChange,
+  required,
+  autoComplete,
+  id,
+  textLabel,
+}: InputProps) => {
+  return (
+    <>
+      <label htmlFor={htmlFor}>{textLabel}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        required={required}
+        autoComplete={autoComplete}
+        id={id}
+      />
+    </>
+  );
+};
+
+export default Input;
